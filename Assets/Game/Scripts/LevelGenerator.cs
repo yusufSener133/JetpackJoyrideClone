bb495880c76248293fdfba54951d0bcc3abe5919
YusufSener <yusuf_sener_97@hotmail.com>
2023-01-05 18:32:26 +0300

    05012023

--- Assets/Game/Scripts/LevelGenerator.cs
+++ Assets/Game/Scripts/LevelGenerator.cs
@@ -6,37 +6,42 @@ public class LevelGenerator : MonoBehaviour
 {
     static LevelGenerator _instance;
     public static LevelGenerator Instance => _instance;
-    [SerializeField] GameObject[] Levels;
-    [SerializeField] int _poolSize;
 
-    Vector3 pos = new Vector3(0, 0, 0);
+
+    [SerializeField] GameObject[] Parts;
+    [SerializeField] int _poolSize = 3;
+
+    Vector3 _lastPos = Vector3.zero;
 
     Dictionary<int, List<GameObject>> _pools = new Dictionary<int, List<GameObject>>();
     void Awake()
     {
         _instance = this;
 
-        for (int i = 0; i < Levels.Length; i++)
+
+
+        for (int i = 0; i < Parts.Length; i++)
         {
             var pool = new List<GameObject>();
-            for (int j = 0; i < _poolSize; j++)
+
+            for (int j = 0; j < _poolSize; j++)
             {
-                var part = Instantiate(Levels[i], transform);
+                var part = Instantiate(Parts[i], transform);
                 part.SetActive(false);
                 pool.Add(part);
             }
 
-            _pools.Add(i, new List<GameObject>());
+            _pools.Add(i, pool);
         }
 
-        for (int i = 0; i < Levels.Length; i++)
+        for (int i = 0; i < Parts.Length; i++)
         {
             CreateLevel(i);
         }
     }
     public void CreateRandomPart()
     {
-        var type = Random.Range(0, Levels.Length);
+        var type = Random.Range(0, Parts.Length);
         CreateLevel(type);
     }
     public void CreateLevel(int type)
@@ -44,8 +49,8 @@ public class LevelGenerator : MonoBehaviour
         var part = _pools[type][0];
         _pools[type].Remove(part);
         part.SetActive(true);
-        pos.x += 41;
-        part.transform.position = pos;
+        _lastPos.x += 41;
+        part.transform.position = _lastPos;
     }
 
     public void AddToPool(int type, GameObject part)
